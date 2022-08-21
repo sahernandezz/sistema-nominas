b9ebb79056ac767f9941baeca41fec9ab5519d1c
alejo <alejo.80.123@hotmail.com>
2022-08-21 17:31:43 -0500

    Validar archivo

--- src/main/resources/static/iu/src/app/pages/login/login.component.ts
+++ src/main/resources/static/iu/src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import {AppComponent} from '../../app.component';
 import {Injectable} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import {LoginHttp} from '../../shared/services/login';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-login',
@@ -16,8 +17,9 @@ import {LoginHttp} from '../../shared/services/login';
 export class LoginComponent implements OnInit, OnDestroy {
   app: AppComponent;
   buttonSummit: Boolean;
+  mensaje: String = '';
 
-  constructor(private route: ActivatedRoute,
+  constructor(private route: ActivatedRoute, public modalService: NgbModal,
               private router: Router, private httpLogin: LoginHttp) {
     this.app = new AppComponent();
     this.buttonSummit = false;
@@ -33,30 +35,39 @@ export class LoginComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
   }
 
+  open(content: any) {
+    this.modalService.open(content, {centered: true, ariaLabelledBy: 'modal-basic-title', backdrop: 'static'});
+  }
+
   private login(auth: Auth, usuario: HTMLInputElement,
                 group_usuario: HTMLInputElement, group_clave: HTMLInputElement,
-                clave: HTMLInputElement): void {
+                clave: HTMLInputElement, modalMensaje: any): void {
     this.buttonSummit = true;
     this.httpLogin.login(auth).subscribe((data) => {
       localStorage.setItem('USER_KEY', data.object.token);
+
       group_usuario.classList.add('has-success');
       group_clave.classList.add('has-success');
+
       usuario.disabled = true;
       clave.disabled = true;
       this.app.setUsuario(data.object['usuario']);
+
       setTimeout(() => {
         this.router.navigate([this.app.getRoutes()[0].path]).then(e => null);
       }, 1000);
     }, (error) => {
-      if (error.status === 400) {
-        this.buttonSummit = false;
-        group_usuario.classList.add('has-danger');
-        group_clave.classList.add('has-danger');
+      this.buttonSummit = false;
+      group_usuario.classList.add('has-danger');
+      group_clave.classList.add('has-danger');
+
+      if (error.status === 0 || error.status === 500) {
+        this.mensajeModal(modalMensaje, "Hay inconvenientes intente nuevamente más tarde");
       }
     });
   }
 
-  datosUsuario(): void {
+  datosUsuario(modalMensaje: any): void {
     try {
       const usuario = document.getElementById('input_login_usuario') as HTMLInputElement || null;
       const group_usuario = document.getElementById('form-group_login_usuario') as HTMLInputElement || null;
@@ -70,10 +81,15 @@ export class LoginComponent implements OnInit, OnDestroy {
         this.login({
           login: usuario.value,
           clave: clave.value
-        }, usuario, group_usuario, group_clave, clave);
+        }, usuario, group_usuario, group_clave, clave, modalMensaje);
       }
     } catch (e) {
       this.buttonSummit = false;
     }
+  }
+
+  mensajeModal(content: any, mensaje: string): void {
+    this.open(content);
+    this.mensaje = mensaje;
   }
 }
